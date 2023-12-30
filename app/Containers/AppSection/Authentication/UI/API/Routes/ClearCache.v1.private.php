8fd506ef0cbffe91ba916756bc836c6d4f3a424a
Vedant <vedantdevelops@gmail.com>
2023-12-31 02:10:00 +0530

    Project Uploaded To Git

---
+++ app/Containers/AppSection/Authentication/UI/API/Routes/ClearCache.v1.private.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @apiGroup           Authentication
+ *
+ * @apiName            Clear Cache
+ *
+ * @api                {GET} Clear Cache
+ *
+ *
+ * {}
+ */
+
+use App\Containers\AppSection\Authentication\UI\API\Controllers\VerifyEmailController;
+use Illuminate\Support\Facades\Route;
+
+    Route::get('clearcache',function(){
+        Artisan::call('cache:clear');
+        Artisan::call('route:clear');
+        Artisan::call('config:clear');
+        Artisan::call('view:clear');
+        return "Cache cleared successfully";
+    });
+    
+
